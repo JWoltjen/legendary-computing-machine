8e0fdf5d1f36d3d22b952309adc8979a73e38b38
Jeff Woltjen <jeff.woltjen@gmail.com>
2021-05-04 13:45:04 -0500

    style chatbot avatar

---
+++ src/config.js
@@ -0,0 +1,8 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+
+const config = {
+  initialMessages: [createChatBotMessage(`Hello, I'm here to chat with you.`)],
+  botName: "Neumann",
+}
+
+export default config
